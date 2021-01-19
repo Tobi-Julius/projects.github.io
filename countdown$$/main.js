35482e61b341521d8fff6c949aec030964f37b42
Tobi-Julius <70627316+Tobi-Julius@users.noreply.github.com>
2021-01-19 09:02:25 +0100

    Add files via upload

---
+++ countdown$$/main.js
@@ -0,0 +1,42 @@
+var countDownDate = new Date ('jan 30, 2021 00:00:00').getTime();
+
+var updateSecond = setInterval(function(){
+
+    var now = new Date().getTime()
+
+    var distance = countDownDate - now
+
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) /(1000 * 60 * 60));
+
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+
+    var seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+    document.getElementById('day').innerText = days
+
+    document.getElementById('hour').innerText = hours
+
+    document.getElementById('minute').innerText = minutes
+
+    document.getElementById('second').innerText = seconds
+
+    if (distance < 1){
+        clearInterval(updateSecond);
+        document.getElementById('day').innerHTML = 'EX'
+        document.getElementById('hour').innerHTML = 'PI'
+        document.getElementById('minute').innerHTML = 'RE'
+        document.getElementById('second').innerHTML = 'D';
+    }
+
+
+}, 1000);
+
+
+
+
+
+
+
+
